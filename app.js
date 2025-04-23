be3f33e319c55a893fac065f62a819b889a2cf8c
akash rathour <akkurathour2@gmail>
2025-04-23 18:29:39 +0530

    added Add to cart Functionality

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+ User.fetchById('6807aa86b34c023b392c2c59').then(user=>{
+  const newuser=new User(user.name,user.email,user.cart,user._id)
+  req.user=newuser;
   next()
+ })
 });
 
  app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,47 @@
-const Sequelize = require('sequelize');
+const { ObjectId } = require('mongodb');
+const { getDb } = require('../util/database')
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email, cart, userID) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+    this.userID = userID;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static fetchById(id) {
+    const db = getDb()
+    return db.collection('User').findOne({ _id: new ObjectId(id) })
+  }
+  addToCart(product) {
+    if (this.cart === undefined) {
+      const updatedCart = { items: [{ productId: new ObjectId(product[0]._id), quantity: 1 }] }
+      const db = getDb();
+      db.collection('User').updateOne({ _id: new ObjectId(this.userID) }, { $set: { cart: updatedCart } })
+    }
+    else {
+
+      const productIndex = this.cart.items.findIndex(p => {
+        return product[0]._id.equals(p.productId)
+      })
+      console.log(productIndex)
+      let newQuantity = 1;
+      if (productIndex >= 0) {
+        // newQuantity = this.cart.items[productIndex].quantity + 1
+        // const updatedCart = { items: [{ productId: new ObjectId(product[0]._id), quantity: newQuantity }] };
+        this.cart.items[productIndex].quantity+=1;
+        const db = getDb();
+        db.collection('User').updateOne({ _id: new ObjectId(this.userID) }, { $set: { cart: this.cart } })
+      }
+      else {
+        const updatedCart = { items: [...this.cart.items, { productId: new ObjectId(product[0]._id), quantity: newQuantity }] }
+        const db = getDb();
+        db.collection('User').updateOne({ _id: new ObjectId(this.userID) }, { $set: { cart: updatedCart } })
+
+      }
+    }
+  }
+
+}
 
 module.exports = User;
